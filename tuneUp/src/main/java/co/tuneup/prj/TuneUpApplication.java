830f511e2e1948124f2f79c4cb57d219696d923e
soom5 <soom5@gmail.com>
2022-09-14 13:52:45 +0900

    최종프로젝트

---
+++ tuneUp/src/main/java/co/tuneup/prj/TuneUpApplication.java
@@ -0,0 +1,13 @@
+package co.tuneup.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneUpApplication.class, args);
+	}
+
+}

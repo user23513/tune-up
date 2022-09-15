48662b6e5a4caf88016720b14e90000866e84004
soom5 <soom5@gmail.com>
2022-09-15 13:30:15 +0900

    부트스트랩 넣었다

--- tuneUp/src/main/java/co/tuneup/prj/TuneUpApplication.java
+++ tuneUp/src/main/java/co/tuneup/prj/TuneUpApplication.java
@@ -3,13 +3,21 @@ package co.tuneup.prj;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @MapperScan(basePackages = "co.tuneup.prj.**.mapper")
+@Controller
 public class TuneUpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TuneUpApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String main() {
+		return "main/main";
 	}
 
 }

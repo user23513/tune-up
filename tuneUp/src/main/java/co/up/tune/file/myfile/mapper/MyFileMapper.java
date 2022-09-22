be9db18d109a469e638097d68c20d1eba3d27090
gimgayun <gimgayun@192.168.0.169>
2022-09-22 20:31:21 +0900

    000

--- tuneUp/src/main/java/co/up/tune/file/myfile/mapper/MyFileMapper.java
+++ tuneUp/src/main/java/co/up/tune/file/myfile/mapper/MyFileMapper.java
@@ -1,12 +1,9 @@
 package co.up.tune.file.myfile.mapper;
 
-import java.util.List;
 
-import co.up.tune.file.vo.FileVO;
 
 public interface MyFileMapper {
 	
-	List<FileVO> myFileList();
-	FileVO my
+
 
 }

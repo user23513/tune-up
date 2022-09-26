65c9aba4e587e4f533c7c90ac43498310904637a
nxbetr <nxbetr@gmail.com>
2022-09-27 00:21:19 +0900

    0927

---
+++ tuneUp/src/main/java/co/up/tune/emp/attdUp/web/AttdUpAjaxController.java
@@ -0,0 +1,27 @@
+package co.up.tune.emp.attdUp.web;
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.up.tune.emp.attdUp.service.AttdUpService;
+import co.up.tune.emp.vo.AttdUpVO;
+
+
+@RestController
+public class AttdUpAjaxController {
+	/*
+	 * @Autowired AttdUpService dao;
+	 */
+ 
+	/*
+	 * @PostMapping("/attdUpOk") public String attdUpOk(AttdUpVO vo,Model model) {
+	 * dao.attdUpOk(vo); model.addAttribute("attdUpOk",dao.attdUpOk(vo)); return
+	 * "redirect:/attdList"; }
+	 */
+	
+}

2610b4cf50453bc18d78e8ab5cf44246153e4b4d
nxbetr <nxbetr@gmail.com>
2022-09-27 14:59:31 +0900

    수정폼 타임스탬프 수정 + 리스트 변환까지 완

--- tuneUp/src/main/java/co/up/tune/emp/vo/AttdUpVO.java
+++ tuneUp/src/main/java/co/up/tune/emp/vo/AttdUpVO.java
@@ -1,7 +1,11 @@
 package co.up.tune.emp.vo;
 
-import java.sql.Date;
-import java.sql.Timestamp;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 @Data
@@ -9,12 +13,20 @@ public class AttdUpVO {
 	int attdupNo; 	//신청번호
 	String empNo; 	//사번
 	String nm; 	//이름
-	Date atdcDt; 	//출근날짜
-	Timestamp atdcDttm; 	//출근시간
-	Timestamp afwkDttm; 	//퇴근시간
+	
+	java.sql.Date atdcDt; 	//출근날짜
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	Date atdcDttm; 	//출근시간
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	Date afwkDttm; 	//퇴근시간
+	
 	String aContent; 	//사유
 	String st;	//상태
-	Timestamp aDate;  	//신청일자
+	java.sql.Date aDate;  	//신청일자
 	
 	
 	

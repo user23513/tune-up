333898d08feb67bc664e89726d05a73fa4865a28
gayun82 <gayun0820@naver.com>
2022-09-28 14:10:26 +0900

    ㅇㅇㅇㅇ

--- tuneUp/src/main/java/co/up/tune/emp/singUp/email/AccountController.java
+++ tuneUp/src/main/java/co/up/tune/emp/singUp/email/AccountController.java
@@ -3,7 +3,6 @@ package co.up.tune.emp.singUp.email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 

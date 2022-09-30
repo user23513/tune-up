7370ddc4c87eda353d083660931eccb71e393711
yun <primula374@naver.com>
2022-09-30 17:34:13 +0900

    220930

--- tuneUp/src/main/java/co/up/tune/aprv/aprvReq/web/AjaxAprvReqController.java
+++ tuneUp/src/main/java/co/up/tune/aprv/aprvReq/web/AjaxAprvReqController.java
@@ -1,13 +1,12 @@
 package co.up.tune.aprv.aprvReq.web;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import co.up.tune.aprv.aprvReq.service.AprvReqService;
 import co.up.tune.aprv.vo.AprvVO;
+import co.up.tune.aprv.vo.FormVO;
 import co.up.tune.common.service.CommonService;
 
 @RestController
@@ -21,9 +20,15 @@ public class AjaxAprvReqController {
 	@RequestMapping("/aprvView")	
 	public AprvVO aprvView(AprvVO vo) {
 		
-		vo= ap.aprvReqSelect(vo);
 		
-		return vo;
+		return ap.aprvReqSelect(vo);
+			
+		}
+	
+	@RequestMapping("/formView")	
+	public FormVO formView(FormVO vo) {
+	
+		return ap.formSelect(vo);
 			
 		}
 		
--- tuneUp/src/main/java/co/up/tune/aprv/vo/FormVO.java
+++ tuneUp/src/main/java/co/up/tune/aprv/vo/FormVO.java
@@ -7,8 +7,8 @@ import lombok.Setter;
 @Setter
 public class FormVO { //결재서식
 	int formNo; //서식번호
-	String formNm; //서식이름
-	String formCntn; //서식설명
+	String ttl; //서식이름
+	String cntn; //서식설명
 	String formCat; //서식종류
 	String empNo; //사번
 	String formAuth; //공개여부

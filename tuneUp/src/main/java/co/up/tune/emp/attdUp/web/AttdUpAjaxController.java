953d9b14a237f527ef0e39978af767b04fd5a334
nxbetr <nxbetr@gmail.com>
2022-10-05 18:00:11 +0900

    1005학원 선생님이랑 수정

--- tuneUp/src/main/java/co/up/tune/emp/attdUp/web/AttdUpAjaxController.java
+++ tuneUp/src/main/java/co/up/tune/emp/attdUp/web/AttdUpAjaxController.java
@@ -3,13 +3,8 @@ package co.up.tune.emp.attdUp.web;
 
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import co.up.tune.emp.attdUp.service.AttdUpService;
-import co.up.tune.emp.vo.AttdUpVO;
 
 
 @RestController

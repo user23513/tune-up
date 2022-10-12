3474987713be7747a01bcaded90953386eb99ee2
soom5 <soom5@gmail.com>
2022-10-12 15:23:24 +0900

    업무 상태 수정

--- tuneUp/src/main/java/co/up/tune/prj/business/web/BusinessController.java
+++ tuneUp/src/main/java/co/up/tune/prj/business/web/BusinessController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -51,5 +52,12 @@ public class BusinessController {
 		re.addAttribute("prjNo", vo.getPrjNo());
 		
 		return "redirect:prjPostList";
+	}
+	
+	//업무 상태 수정
+	@ResponseBody
+	@PostMapping("/stUpdate")
+	public int stUpdate(BusinessVO vo) {
+		return dao.busStUpdate(vo);
 	}
 }
